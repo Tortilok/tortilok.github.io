b2bcdbd6853a229f8454732a8166e822b018d5a9
Tortik <domashkin2120@gmail.com>
2021-02-25 23:04:07 +0300

    23

--- js/dispatcher.js
+++ js/dispatcher.js
@@ -3,7 +3,7 @@ function Parser() {
 
   if (command == "show") {
     document.getElementById("spinningTheWheel").style.display = 'flex';
-    document.getElementById("main_text").innerHTML = "Вот тебе небольшая подсказка.<br>Жду тебя";
+    document.getElementById("main_text").innerHTML = "Вот тебе колесо помощник.<br>";
     document.getElementById("vvod").value = "";
 
     const TypeExternal = new CircleType(document.getElementById('text1'));
@@ -12,11 +12,11 @@ function Parser() {
     TypeInternal.radius().dir(1);
   }
   else if (command == "help") {
-
+    document.getElementById("main_text").innerHTML = "Доступные команды:<br>   ping - проверить связь с другим компьютером. Пример: ping 192.168.1.1 <br>  all - показывает IP адреса ПК.<br>   ifconfig - узнать свой IP адрес.<br>   ";
   }
   else if (command == "ifconfig") {
     document.getElementById("ipAdress").style.display = 'flex';
-    document.getElementById("main_text").innerHTML = "Твой IP адрес: 192.168.5.103<br>Маска сети: 255.255.255.0<br>Чтобы увидеть список команд напиши команду: help";
+    document.getElementById("main_text").innerHTML = "Твой IP адрес: 192.168.5.103<br>Чтобы увидеть список команд напиши команду: help";
   }
   else if (command == "ping 192.168.5.122") {
 
@@ -28,9 +28,12 @@ function Parser() {
 
   }
   else if (command == "all") {
-    document.getElementById("main_text").innerHTML = "ПК1: 192.168.5.122<br>ПК2: 127.200.35.1<br>ПК3: 157.182.77.3<br><br>Чтобы увидеть список команд напиши команду: help";
+    document.getElementById("main_text").innerHTML = "ПК1: 192.168.5.122<br>ПК2: 127.200.35.1<br>ПК3: 157.182.77.3<br>Чтобы увидеть список команд напиши команду: help";
   }
   else if (command == "msg") {
+
+  }
+  else {
 
   }
 
