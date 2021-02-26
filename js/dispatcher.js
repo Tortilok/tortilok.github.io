2430c220774fbdad422a710bbc75a8cfcb918827
Tortik <domashkin2120@gmail.com>
2021-02-26 08:22:51 +0300

    fin govno

--- js/dispatcher.js
+++ js/dispatcher.js
@@ -1,9 +1,9 @@
 function Parser() {
-  command = document.getElementById("vvod").value;
+  var command = document.getElementById("vvod").value;
 
   if (command == "show") {
     document.getElementById("spinningTheWheel").style.display = 'flex';
-    document.getElementById("main_text").innerHTML = "Вот тебе колесо помощник.<br>";
+    document.getElementById("main_text").innerHTML = "Вот тебе колесо помощник в решение моей задачки.<br>";
     document.getElementById("vvod").value = "";
 
     const TypeExternal = new CircleType(document.getElementById('text1'));
@@ -11,30 +11,83 @@ function Parser() {
     TypeExternal.radius().dir(1);
     TypeInternal.radius().dir(1);
   }
+
   else if (command == "help") {
-    document.getElementById("main_text").innerHTML = "Доступные команды:<br>   ping - проверить связь с другим компьютером. Пример: ping 192.168.1.1 <br>  all - показывает IP адреса ПК.<br>   ifconfig - узнать свой IP адрес.<br>   ";
+    document.getElementById("main_text").innerHTML = "Доступные команды:<br>ping - проверить связь с другим компьютером. Пример: ping 192.168.1.1 <br>all - показывает IP адреса ПК.<br>ifconfig - узнать свой IP адрес.<br>msg - отправить сообщение по IP аресу. Пример: msg 192.168.5.122 привет";
   }
+
   else if (command == "ifconfig") {
     document.getElementById("ipAdress").style.display = 'flex';
     document.getElementById("main_text").innerHTML = "Твой IP адрес: 192.168.5.103<br>Чтобы увидеть список команд напиши команду: help";
   }
-  else if (command == "ping 192.168.5.122") {
 
+  else if (command.indexOf("ping") == 0) {
+    temp = command.split(" ");
+    if (temp[1] == "192.168.5.122") {
+      document.getElementById("box1").style.display = 'flex';
+      document.getElementById("main_text").innerHTML = "Проверка соединения:<br>";
+      for (var i = 0; i < 5; i++) {
+        var text = document.getElementById("main_text").innerHTML;
+        document.getElementById("main_text").innerHTML = text + i +": Соединение успешно. Задержка: " + (10+i-i*2) + ".<br>";
+      }
+    }
+    else if (temp[1] == "127.200.35.1") {
+      document.getElementById("box2").style.display = 'flex';
+      document.getElementById("main_text").innerHTML = "Проверка соединения:<br>";
+      for (var i = 0; i < 5; i++) {
+        var text = document.getElementById("main_text").innerHTML;
+        if (i < 3) {
+          document.getElementById("main_text").innerHTML = text + i +": Соединение успешно. Задержка: " + (1000-i) + ".<br>";
+        }
+        else {
+          document.getElementById("main_text").innerHTML = text + i +": Соединение неустановлено.<br>";
+        }
+      }
+    }
+    else if (temp[1] == "157.182.77.3") {
+      document.getElementById("box3").style.display = 'flex';
+      document.getElementById("main_text").innerHTML = "Проверка соединения:<br>";
+      for (var i = 0; i < 5; i++) {
+        var text = document.getElementById("main_text").innerHTML;
+        document.getElementById("main_text").innerHTML = text + i +": Соединение неустановлено.<br>";
+      }
+    }
+    else if (temp[1] == "192.168.5.103") {
+      document.getElementById("ipAdress").style.display = 'flex';
+      document.getElementById("main_text").innerHTML = "Проверка соединения:<br>";
+      for (var i = 0; i < 5; i++) {
+        var text = document.getElementById("main_text").innerHTML;
+        document.getElementById("main_text").innerHTML = text + i +": Соединение успешно. Задержка: " + 1 + ".<br>";
+      }
+    }
+    else {
+      document.getElementById("main_text").innerHTML = "Такого IP адреса нету.<br> Для просмотра списка IP адресов наберите команду: all<br>Чтобы увидеть список команд напиши команду: help";
+    }
   }
-  else if (command == "ping 127.200.35.1") {
 
-  }
-  else if (command == "ping 157.182.77.3") {
-
-  }
   else if (command == "all") {
-    document.getElementById("main_text").innerHTML = "ПК1: 192.168.5.122<br>ПК2: 127.200.35.1<br>ПК3: 157.182.77.3<br>Чтобы увидеть список команд напиши команду: help";
+    document.getElementById("main_text").innerHTML = "ПК1: 192.168.5.122<br>ПК2: 127.200.35.1<br>ПК3: 157.182.77.3<br>ПК4: 192.168.5.103<br>Чтобы увидеть список команд напиши команду: help";
   }
-  else if (command == "msg") {
 
+  else if (command.indexOf("msg") == 0) {
+    temp = command.split(" ");
+
+    if (temp[1] == "192.168.5.122") {
+      //8 сдвиг
+      if (temp[2] == "мутз") {
+        document.getElementById("win").style.display= "flex";
+      }
+      else {
+        document.getElementById("main_text").innerHTML = "Ты чего обзываешься? Кажется твои сообщения были перехвачены.<br>Я буду с тобой общаться после решение моей загадки:<br>Прфцс р умъцф цлхрф юймъцф?<br>Для вызова помощника напиши show.";
+      }
+    }
+    else {
+      document.getElementById("main_text").innerHTML = "Не могу доставить сообщение, соединение неустановлено.";
+    }
   }
+
   else {
-
+    document.getElementById("main_text").innerHTML = "Такой команды нету.<br>Чтобы увидеть список команд напиши команду: help";
   }
 
   document.getElementById("vvod").value = "";
--- js/listener.js
+++ js/listener.js
@@ -5,3 +5,31 @@ command.addEventListener("keyup", function(event) {
     document.getElementById("button").click();
   }
 });
+
+const body = document.querySelector('body');
+const scrollingTheWheel = document.getElementById('text2');
+const scrollingTheWheelContainer = document.getElementById('spinningTheWheel');
+let x = 0;
+var count = 33;
+
+body.addEventListener("keyup", function(event) {
+  if (event.keyCode == 39) {
+    x = x + 11;
+    count ++;
+    scrollingTheWheel.style.transform = 'rotate(' + x + 'deg)'
+    document.getElementById('count').innerHTML = count % 33;
+  }
+  else if (event.keyCode == 37) {
+    x = x - 11;
+    count --;
+    scrollingTheWheel.style.transform = 'rotate(' + x + 'deg)'
+    document.getElementById('count').innerHTML = count % 33;
+  }
+});
+
+scrollingTheWheelContainer.onclick = () => {
+    count ++;
+    x = x + 11;
+    scrollingTheWheel.style.transform = 'rotate(' + x + 'deg)'
+    document.getElementById('count').innerHTML = count % 33;
+}
